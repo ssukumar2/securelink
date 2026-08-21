4e34327ab265b10ed99b371cebc910483c96f649
Sairam Sukumar <s.sukumar.eu@gmail.com>
2026-08-21 23:02:37 +0200

    refactor 6 files to use shared sl_posix_compat.h

--- src/sl_event_log.c
+++ src/sl_event_log.c
@@ -1,8 +1,5 @@
 #include "sl_posix_compat.h"
-/* clock_gettime and CLOCK_REALTIME are POSIX, not standard C11 -- same
- * pattern as sl_lockout.c, sl_dos_guard.c, sl_token_bucket.c, sl_clock.c. */
-#define _POSIX_C_SOURCE 200809L
-
+#include "sl_posix_compat.h"
 #include "sl_event_log.h"
 
 #include <stdio.h>
